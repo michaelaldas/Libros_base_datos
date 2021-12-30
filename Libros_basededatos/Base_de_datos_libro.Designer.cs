535d51435d0a930941bec679cc3424a2952d0464
michaelaldas <vmaldas@pucese.edu.ec>
2021-12-30 16:34:08 -0500

    Agregar archivos de proyecto.

---
+++ Libros_basededatos/Base_de_datos_libro.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace Libros_basededatos
+{
+    partial class Base_de_datos_libro
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.datoFecha = new System.Windows.Forms.DateTimePicker();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.bntIngresar = new System.Windows.Forms.Button();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboLibro = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.VerDatoFecha = new System.Windows.Forms.DateTimePicker();
+            this.textVerNombre = new System.Windows.Forms.TextBox();
+            this.textVerCodigo = new System.Windows.Forms.TextBox();
+            this.textVerPrecio = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.datoFecha);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.bntIngresar);
+            this.groupBox1.Controls.Add(this.txtCodigo);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(27, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(415, 302);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "AGREGAR LIBROS";
+            // 
+            // datoFecha
+            // 
+            this.datoFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datoFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datoFecha.Location = new System.Drawing.Point(249, 160);
+            this.datoFecha.MaxDate = new System.DateTime(2021, 12, 28, 0, 0, 0, 0);
+            this.datoFecha.Name = "datoFecha";
+            this.datoFecha.Size = new System.Drawing.Size(160, 20);
+            this.datoFecha.TabIndex = 7;
+            this.datoFecha.Value = new System.DateTime(2021, 12, 28, 0, 0, 0, 0);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(249, 83);
+            this.txtNombre.MaxLength = 50;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(160, 20);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // bntIngresar
+            // 
+            this.bntIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntIngresar.Location = new System.Drawing.Point(134, 202);
+            this.bntIngresar.Name = "bntIngresar";
+            this.bntIngresar.Size = new System.Drawing.Size(116, 58);
+            this.bntIngresar.TabIndex = 10;
+            this.bntIngresar.Text = "Ingresar";
+            this.bntIngresar.UseVisualStyleBackColor = true;
+            this.bntIngresar.Click += new System.EventHandler(this.bntIngresar_Click);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.Location = new System.Drawing.Point(249, 50);
+            this.txtCodigo.MaxLength = 10;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(160, 20);
+            this.txtCodigo.TabIndex = 5;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(249, 121);
+            this.txtPrecio.MaxLength = 10;
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(160, 20);
+            this.txtPrecio.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(19, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fecha de compra del libro:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Precio del libro:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre del libro:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo del libro:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(-3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "BUSCAR LIBROS";
+            // 
+            // comboLibro
+            // 
+            this.comboLibro.FormattingEnabled = true;
+            this.comboLibro.Location = new System.Drawing.Point(8, 31);
+            this.comboLibro.Name = "comboLibro";
+            this.comboLibro.Size = new System.Drawing.Size(365, 24);
+            this.comboLibro.TabIndex = 9;
+            this.comboLibro.SelectedIndexChanged += new System.EventHandler(this.Mostrar);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnActualizar);
+            this.groupBox2.Controls.Add(this.btnEliminar);
+            this.groupBox2.Controls.Add(this.VerDatoFecha);
+            this.groupBox2.Controls.Add(this.textVerNombre);
+            this.groupBox2.Controls.Add(this.comboLibro);
+            this.groupBox2.Controls.Add(this.textVerCodigo);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textVerPrecio);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(460, 60);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(415, 302);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Libros";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Location = new System.Drawing.Point(237, 247);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(91, 38);
+            this.btnActualizar.TabIndex = 12;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(63, 247);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(91, 38);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // VerDatoFecha
+            // 
+            this.VerDatoFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VerDatoFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.VerDatoFecha.Location = new System.Drawing.Point(213, 196);
+            this.VerDatoFecha.MaxDate = new System.DateTime(2021, 12, 28, 0, 0, 0, 0);
+            this.VerDatoFecha.Name = "VerDatoFecha";
+            this.VerDatoFecha.Size = new System.Drawing.Size(160, 20);
+            this.VerDatoFecha.TabIndex = 7;
+            this.VerDatoFecha.Value = new System.DateTime(2021, 12, 28, 0, 0, 0, 0);
+            // 
+            // textVerNombre
+            // 
+            this.textVerNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textVerNombre.Location = new System.Drawing.Point(213, 119);
+            this.textVerNombre.MaxLength = 50;
+            this.textVerNombre.Name = "textVerNombre";
+            this.textVerNombre.Size = new System.Drawing.Size(160, 20);
+            this.textVerNombre.TabIndex = 6;
+            // 
+            // textVerCodigo
+            // 
+            this.textVerCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textVerCodigo.Location = new System.Drawing.Point(213, 72);
+            this.textVerCodigo.MaxLength = 10;
+            this.textVerCodigo.Name = "textVerCodigo";
+            this.textVerCodigo.ReadOnly = true;
+            this.textVerCodigo.Size = new System.Drawing.Size(160, 20);
+            this.textVerCodigo.TabIndex = 5;
+            // 
+            // textVerPrecio
+            // 
+            this.textVerPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textVerPrecio.Location = new System.Drawing.Point(213, 160);
+            this.textVerPrecio.MaxLength = 10;
+            this.textVerPrecio.Name = "textVerPrecio";
+            this.textVerPrecio.Size = new System.Drawing.Size(160, 20);
+            this.textVerPrecio.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(5, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 15);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Fecha de compra del libro:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(4, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 15);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Precio del libro:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(4, 124);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(102, 15);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Nombre del libro:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(5, 77);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 15);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Codigo del libro:";
+            // 
+            // Base_de_datos_libro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 509);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Base_de_datos_libro";
+            this.Text = "Deber Jairon Cardena";
+            this.Load += new System.EventHandler(this.Base_de_datos_libro_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker datoFecha;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboLibro;
+        private System.Windows.Forms.Button bntIngresar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DateTimePicker VerDatoFecha;
+        private System.Windows.Forms.TextBox textVerNombre;
+        private System.Windows.Forms.TextBox textVerCodigo;
+        private System.Windows.Forms.TextBox textVerPrecio;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ Libros_basededatos/Base_de_datos_libro.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Libros_basededatos
+{
+    public partial class Base_de_datos_libro : Form
+    {
+        public Base_de_datos_libro()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conexion = new SqlConnection(@"server=DESKTOP-DC0P3Q7\SQLEXPRESS; database=Libro de Base de Datos; Integrated Security=true");
+
+        private void bntIngresar_Click(object sender, EventArgs e)
+        {
+                conexion.Open();
+                string insertar = "INSERT INTO TablaLibro (Cod_Libro, NombreLibro, PrecioCompra, FechaCompra )VALUES(@Cod_Libro, @NombreLibro, @PrecioCompra, @FechaCompra )";
+                SqlCommand comando = new SqlCommand(insertar, conexion);
+                comando.Parameters.Add(new SqlParameter("@Cod_Libro", this.txtCodigo.Text));
+                comando.Parameters.Add(new SqlParameter("@NombreLibro", this.txtNombre.Text));
+                comando.Parameters.Add(new SqlParameter("@PrecioCompra", this.txtPrecio.Text));
+                comando.Parameters.Add(new SqlParameter("@FechaCompra", datoFecha.Value));
+                comando.ExecuteNonQuery();
+                conexion.Close();
+                MessageBox.Show("Filas Insertadas Correctamente");
+            textVerCodigo.Clear();
+            textVerPrecio.Clear();
+            textVerNombre.Clear();
+            Base_de_datos_libro_Load(sender, e);
+        }
+
+        private void Base_de_datos_libro_Load(object sender, EventArgs e)
+        {
+            DataTable dt = getpersona();
+            this.comboLibro.DataSource = dt;
+            this.comboLibro.DisplayMember = "NombreLibro";
+            this.comboLibro.ValueMember = "Cod_Libro";
+        }
+        private DataTable getpersona(string codigo = "")
+        {
+            string sql = "";
+            if (codigo == "")
+            {
+                sql = "select Cod_Libro, NombreLibro, PrecioCompra, FechaCompra ";
+                sql += "from TablaLibro order by nombreLibro, Cod_Libro";
+            }
+            else
+            {
+                sql = "select Cod_Libro, NombreLibro, PrecioCompra, FechaCompra ";
+                sql += "from TablaLibro where Cod_Libro=@Cod_Libro order by NombreLibro";
+            }
+
+            SqlCommand comando = new SqlCommand(sql, conexion);
+            if (codigo != "")
+            {
+                comando.Parameters.Add(new SqlParameter("@Cod_Libro", codigo));
+            }
+            SqlDataAdapter ad1 = new SqlDataAdapter(comando);
+
+            //pasar los datos del adaptador a un datatable
+            DataTable dt = new DataTable();
+            ad1.Fill(dt);
+            return dt;
+        }
+        private void Mostrar(object sender, EventArgs e)
+        {
+            //hola
+            DataTable dt = getpersona(this.comboLibro.SelectedValue.ToString());
+            //mostrar la informacion
+            foreach (DataRow row in dt.Rows)
+            {
+                this.textVerCodigo.Text = row["Cod_Libro"].ToString();
+                this.textVerNombre.Text = row["NombreLibro"].ToString();
+                this.textVerPrecio.Text = row["PrecioCompra"].ToString();
+                this.VerDatoFecha.Value = Convert.ToDateTime(row["FechaCompra"].ToString());
+
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("DESE ELIMINAR?", "CONFIRMAR", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                conexion.Open();
+                string eliminar = "DELETE FROM TablaLibro WHERE Cod_Libro = @Cod_Libro";
+                SqlCommand cmd3 = new SqlCommand(eliminar, conexion);
+                cmd3.Parameters.AddWithValue("@Cod_Libro", this.textVerCodigo.Text);
+                cmd3.ExecuteNonQuery();
+                conexion.Close();
+                MessageBox.Show("DATO ELIMINADO CON EXITO", "ELIMINAR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                DataTable dt = getpersona();
+                textVerCodigo.Clear();
+                textVerPrecio.Clear();
+                textVerNombre.Clear();
+                Base_de_datos_libro_Load(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("NO SE ELIMINO NINGUN DATO", "CANCELAR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(" DESEAS ACTUALIZAR?", "CONFIRMAR", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                conexion.Open();
+                string actualizar = "UPDATE TablaLibro SET NombreLibro=@NombreLibro, PrecioCompra=@PrecioCompra, FechaCompra=@FechaCompra WHERE Cod_Libro=@Cod_Libro";
+                SqlCommand cmd2 = new SqlCommand(actualizar, conexion);
+                cmd2.Parameters.AddWithValue("@Cod_Libro", this.textVerCodigo.Text);
+                cmd2.Parameters.AddWithValue("@PrecioCompra", this.textVerPrecio.Text);
+                cmd2.Parameters.AddWithValue("@NombreLibro", this.textVerNombre.Text);
+                cmd2.Parameters.AddWithValue("@FechaCompra", VerDatoFecha.Value);
+                cmd2.ExecuteNonQuery();
+                MessageBox.Show("Actualizacion de Datos");
+                conexion.Close();
+                DataTable dt = getpersona();
+                textVerCodigo.Clear();
+                textVerPrecio.Clear();
+                textVerNombre.Clear();
+                Base_de_datos_libro_Load(sender, e);
+            }
+        }
+    }
+}
